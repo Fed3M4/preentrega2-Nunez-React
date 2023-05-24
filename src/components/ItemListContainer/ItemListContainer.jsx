5813a9f7bebc939a075cef2699be645a4598cafd
Federico <nunez.fedema@gmmail.com>
2023-05-24 16:06:32 -0300

    agregado del loading en itemListContainer

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -2,29 +2,53 @@ import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
 import { itemsBautismo } from "../ItemListContainer/itemsBautismo";
+import React, { useState, useEffect } from "react";
 
-const ItemListContainer = ({agregarAlCarrito}) => {
+const ItemListContainer = ({ agregarAlCarrito }) => {
   const handleClick = () => {
-    agregarAlCarrito()
-  }
+    agregarAlCarrito();
+  };
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
   return (
     <main className="d-flex justify-content-evenly flex-nowrap w-100">
       <div className="card-list d-flex justify-content-evenly align-items-center flex-nowrap w-100">
         {itemsBautismo.map((art) => (
-            <Card style={{ width: "18rem", height: "27rem"}}>
-              <Card.Img variant="top" src={art.img} style={{height: "12rem"}}/>
-              <Card.Body>
-                <Card.Title> <Link to={`/category/bautismos/item/${art.id}`}>{art.id} - {art.articulo}</Link>
-                </Card.Title>
-                <Card.Text>
-                  Para: {art.para} <br />
-                  Precio de: ${art.precio}
-                </Card.Text>
-                <Button variant="primary" onClick={handleClick}>
-                  Comprar
-                </Button>
-              </Card.Body>
-            </Card>
+          <div key={art.id}>
+            {loading ? (
+              <p> Cargando...</p>
+            ) : (
+              <Card style={{ width: "18rem", height: "27rem" }}>
+                <Card.Img
+                  variant="top"
+                  src={art.img}
+                  style={{ height: "12rem" }}
+                />
+                <Card.Body>
+                  <Card.Title>
+                    {" "}
+                    <Link to={`/category/bautismos/item/${art.id}`}>
+                      {art.id} - {art.articulo}
+                    </Link>
+                  </Card.Title>
+                  <Card.Text>
+                    Para: {art.para} <br />
+                    Precio de: ${art.precio}
+                  </Card.Text>
+                  <Button variant="primary" onClick={handleClick}>
+                    Comprar
+                  </Button>
+                </Card.Body>
+              </Card>
+            )}
+          </div>
         ))}
       </div>
     </main>
